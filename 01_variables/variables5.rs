6b33e57a475c4ae40dc97391e17f21faca791546
Matt <matt@cartycodes.com>
2024-10-05 15:43:24 +0900

    finished if

--- 01_variables/variables5.rs
+++ 01_variables/variables5.rs
@@ -3,12 +3,6 @@ fn main() {
     println!("Spell a number: {}", number);
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-<<<<<<< HEAD
-    let number: i32 = 3;
-||||||| 65e764f
-    number = 3;
-=======
     let number: u32 = 3;
->>>>>>> 0fb027df31237610d71c3589f5d9e6fd30578e22
     println!("Number plus two is: {}", number + 2);
 }
--- 03_if/if2.rs
+++ 03_if/if2.rs
@@ -2,13 +2,14 @@
 fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
-    }
+    } else if fizzish == "fuzz" {
+        "bar"
+    } else { "baz" }
 }
 
 fn main() {
     // You can optionally experiment here.
+    foo_if_fizz("bar");
 }
 
 // TODO: Read the tests to understand the desired behavior.
